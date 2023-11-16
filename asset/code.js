1895e5b45f0ee2a83860c4e4fc115558cfe64858
hoangducchung <hoangducchungdtnt@gmail.com>
2023-11-17 05:52:41 +0700

    Add project files.

---
+++ asset/code.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    var table_calendar = `
+    <table class = "table-responsive-lg">
+        <tr>
+            <th>BUỔI</th>
+            <th>THỨ 2</th>
+            <th>THỨ 3</th>
+            <th>THỨ 4</th>
+            <th>THỨ 5</th>
+            <th>THỨ 6</th>
+            <th>THỨ 7</th>
+            <th>CHỦ NHẬT</th>
+        </tr>
+    `;
+
+    for (var i = 1; i <= 24; i++) //24h trong 1 ngày
+        table_calendar += `
+        <tr>
+            <td>    </td>
+            <td>    </td>
+            <td>    </td>
+            <td>    </td>
+            <td>    </td>
+            <td>    </td>
+            <td>    </td>
+            <td>    </td>
+        </tr>`;
+
+    table_calendar += `</table>`;
+    $("#calendar").html(table_calendar);
+});
---
+++ lib/AppSettingsGet.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+namespace QLCV
+{
+    public static class AppSettingsGet
+    {
+        // I also get my connection string from here
+        public static string ConnectionString
+        {
+            get { 
+                return ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString; 
+            }
+        }
+        
+    }
+}
